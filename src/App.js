c547137d7e45c284804ce76e5993857352b8d6ce
Shruti Angane <shrutiangane628@gmail.com>
2023-02-14 23:16:24 +0530

    updated appjs

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import { useEffect } from "react";
 import './index.css';
 
 function App() {
-  useEffect(async()=>{
+  useEffect(()=>{
     let unordered=document.getElementById('language')
     let body=[]
-    // const options = {
-    //   method: 'GET',
-    //   headers: {
-    //     'Accept-Encoding': 'application/gzip',
-    //     'X-RapidAPI-Key': process.env.REACT_APP_TESTING_API,
-    //     'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-    //   }
-    // };
-    
-   const response=await fetch('/.netlify/functions/GetLanguage')
-   body=response.data.languages
-   for (let i=0;i<body.length;i++) {
-      let li = document.createElement("li");
-      li.innerText = body[i].name;
-      unordered.appendChild(li);
+    async function fetchData(){
+      const response=await fetch('/.netlify/functions/GetLanguage')
+      body=response.data.languages
+      for (let i=0;i<body.length;i++) {
+          let li = document.createElement("li");
+          li.innerText = body[i].name;
+          unordered.appendChild(li);
+        }
     }
+    fetchData();
   },[])
   return (
     <>
