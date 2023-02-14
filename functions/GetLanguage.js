841e4fca876980aaf88ff4a767b0505cf06394dd
Shruti Angane <shrutiangane628@gmail.com>
2023-02-14 23:03:34 +0530

    added netlify function logic

---
+++ functions/GetLanguage.js
@@ -0,0 +1,25 @@
+exports.handler= async function(event,callback){
+    const options = {
+        method: 'GET',
+        headers: {
+          'Accept-Encoding': 'application/gzip',
+          'X-RapidAPI-Key': process.env.REACT_APP_TESTING_API,
+          'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+        }
+      }
+      try{
+        const response=await fetch('https://google-translate1.p.rapidapi.com/language/translate/v2/languages?target=en', options);
+        const data=await response.json()
+        return{
+            statusCode:200,
+            body:data
+        }
+      }catch(error){
+        const { status, statusText, headers, data } = error.response
+        return {
+        statusCode: status,
+        body: JSON.stringify({status, statusText, headers, data})
+        }
+      }
+
+}
--- src/App.js
+++ src/App.js
@@ -2,29 +2,25 @@ import { useEffect } from "react";
 import './index.css';
 
 function App() {
-  useEffect(()=>{
+  useEffect(async()=>{
     let unordered=document.getElementById('language')
     let body=[]
-    const options = {
-      method: 'GET',
-      headers: {
-        'Accept-Encoding': 'application/gzip',
-        'X-RapidAPI-Key': process.env.REACT_APP_TESTING_API,
-        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-      }
-    };
+    // const options = {
+    //   method: 'GET',
+    //   headers: {
+    //     'Accept-Encoding': 'application/gzip',
+    //     'X-RapidAPI-Key': process.env.REACT_APP_TESTING_API,
+    //     'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+    //   }
+    // };
     
-    fetch('https://google-translate1.p.rapidapi.com/language/translate/v2/languages?target=en', options)
-      .then(response => response.json())
-      .then(response => {
-        body=response.data.languages
-        for (let i=0;i<body.length;i++) {
-          let li = document.createElement("li");
-          li.innerText = body[i].name;
-          unordered.appendChild(li);
-    }})
-      .catch(err => console.error(err));
-
+   const response=await fetch('/.netlify/functions/GetLanguage')
+   body=response.data.languages
+   for (let i=0;i<body.length;i++) {
+      let li = document.createElement("li");
+      li.innerText = body[i].name;
+      unordered.appendChild(li);
+    }
   },[])
   return (
     <>
